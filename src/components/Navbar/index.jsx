a7123b67bb8ea9b7eb9cc32e946fdb1cb4090b89
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-05-03 09:12:18 +0500

    biroz o'zgarishlar kiritildi

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import "./index.scss";
 
 function Navbar({ setCurruncy, toggleDrawer }) {
-  const [current, setCurrent] = useState((JSON.parse(localStorage.getItem("currency"))));
+  const [current, setCurrent] = useState(
+    JSON.parse(localStorage.getItem("currency"))
+  );
 
   console.log(7, current);
-
 
   return (
     <div className="nav--container">
       <span className="logotip">CRYPTOFOLIO</span>
       <section className="currencyChange">
         <select
-        value={current}
+          value={current}
           onChange={(e) => {
             setCurruncy({ currencies: String(e.target?.value) });
             setCurrent(String(e.target?.value));
@@ -21,24 +22,27 @@ function Navbar({ setCurruncy, toggleDrawer }) {
           name="currency"
           id="currencyChanges"
         >
-          {
-          (current == "USD") && (
+          {(current == "USD" && (
             <>
               <option value="USD">USD</option>
               <option value="EUR">EUR</option>
               <option value="RUB">RUB</option>
-            </> ) || (current == "EUR") && (
-            <>
-              <option value="EUR">EUR</option>
-              <option value="USD">USD</option>
-              <option value="RUB">RUB</option>
-            </> ) || (current == "RUB") && (
-            <>
-              <option value="RUB">RUB</option>
-              <option value="USD">USD</option>
-              <option value="EUR">EUR</option>
-            </> )
-          }
+            </>
+          )) ||
+            (current == "EUR" && (
+              <>
+                <option value="EUR">EUR</option>
+                <option value="USD">USD</option>
+                <option value="RUB">RUB</option>
+              </>
+            )) ||
+            (current == "RUB" && (
+              <>
+                <option value="RUB">RUB</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+              </>
+            ))}
         </select>
         <button
           className="watchlistButton"
